31b23dd60a27a7d7e512f7e91c491da58c079731
egkhayrullina <shanonn@mail.ru>
2026-03-15 18:45:56 +0500

    Dependency Injection

---
+++ 01_di/android/app/src/main/java/ru/netology/nmedia/di/FirebaseModule.kt
@@ -0,0 +1,26 @@
+package ru.netology.nmedia.di
+
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.firebase.messaging.FirebaseMessaging
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+
+    @Provides
+    @Singleton
+    fun provideFirebaseMessaging(): FirebaseMessaging {
+        return FirebaseMessaging.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGoogleApiAvailability(): GoogleApiAvailability {
+        return GoogleApiAvailability.getInstance()
+    }
+}
---
+++ 01_di/android/app/src/main/java/ru/netology/nmedia/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package ru.netology.nmedia.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.netology.nmedia.BuildConfig
+import ru.netology.nmedia.api.PostsApiService
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            if (BuildConfig.DEBUG) {
+                level = HttpLoggingInterceptor.Level.BODY
+            }
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl("${BuildConfig.BASE_URL}/api/slow/")
+            .client(okHttpClient)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePostsApiService(retrofit: Retrofit): PostsApiService {
+        return retrofit.create(PostsApiService::class.java)
+    }
+}
