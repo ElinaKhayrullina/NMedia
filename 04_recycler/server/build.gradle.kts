b506d19ae8a5a71b21638f81a55366bb44b5c69c
egkhayrullina <shanonn@mail.ru>
2026-03-29 00:06:00 +0500

    Paging Refresh, Prepend & Append

---
+++ 04_recycler/server/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("org.springframework.boot") version "4.0.1"
+    id("io.spring.dependency-management") version "1.1.7"
+    val kotlinVersion = "2.3.0"
+    id("org.jetbrains.kotlin.plugin.jpa") version kotlinVersion
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+}
+
+group = "ru.netology"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("com.github.javafaker:javafaker:1.0.2") {
+        exclude(module = "snakeyaml")
+    }
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.apache.tika:tika-core:3.2.3")
+    implementation("com.google.firebase:firebase-admin:9.7.0")
+    runtimeOnly("com.h2database:h2")
+    implementation("org.bouncycastle:bctls-jdk15on:1.70")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
